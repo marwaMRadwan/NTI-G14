39375b71875154fe08268fd37d0b83c534a6b046
marwaMRadwan <marwaradwan872@hotmail.com>
2022-01-02 13:58:28 +0200

    session1

--- session1/tasksApp/js/task.js
+++ session1/tasksApp/js/task.js
@@ -1,6 +1,14 @@
 let x = document.querySelector('div')
+const showHideBtn =document.querySelector("#showHide")
 // x.innerText = "hi from dom"
-document.querySelector("#showHide").addEventListener('click', function(e){
-    //console.log(e.target)
+showHideBtn.addEventListener('click', function(e){
+    // console.log(e)
+    showHideBtn.textContent == "Show" ? showHideBtn.textContent = "Hide" : showHideBtn.textContent = "Show"
+    /*
+    if(showHideBtn.textContent == "Show" ) showHideBtn.textContent = "Hide" 
+    else showHideBtn.textContent = "Show"
+     */
     x.classList.toggle('d-none')
 })
+
+data = ['t1', 't2', 't3']
