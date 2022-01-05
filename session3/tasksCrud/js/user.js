53f94b32d81f54d45fec7eca444100088af019d3
marwaMRadwan <marwaradwan872@hotmail.com>
2022-01-05 11:15:42 +0200

    s3

--- session3/tasksCrud/js/user.js
+++ session3/tasksCrud/js/user.js
@@ -14,18 +14,21 @@ const setDataToStorage = (myData) => {
     myData = JSON.stringify(myData)
     localStorage.setItem('users', myData)
 }
-const usersData = readDataFromStorage()
+const content= document.querySelector("#content")
 const addUser = document.querySelector('#addUser')
-userMainHeads = [
+
+const userMainHeads = [
     {name:"id",dataStore:null,  default: Date.now(), isDefault:true},
     {name:"username", dataStore:"value",default:null, isDefault:false},
     {name:"email", dataStore:"value",default:null, isDefault:false},
     {name:"gender", dataStore:"value",default:null, isDefault:false},
     {name:"isMarried", dataStore:"checked",default:null, isDefault:false},
 ]
+
 if(addUser){
     addUser.addEventListener("submit", function(e){
         e.preventDefault()
+        const usersData=readDataFromStorage()
         const user = {}
         userMainHeads.forEach(head => {
             if(head.isDefault) user[head.name]= Date.now()
@@ -49,25 +52,29 @@ const createMyOwnElement = (element, parent, classes="", textContent="",attribut
     return el
 }
 
-const content= document.querySelector("#content")
-if(content){
+drawItems = () =>{
+    content.innerHTML=""
+    const usersData=readDataFromStorage()
     if(usersData.length==0){
         const tr = createMyOwnElement('tr',content, "alert alert-danger text-center")
         createMyOwnElement('td', tr,"", "No Users Yet", [{attName:"colspan", attrVal:6}] )
-}
+    }
     else{
-     usersData.forEach((user, index)=>{
+     usersData.forEach((user)=>{
       const tr = createMyOwnElement('tr',content)
       userMainHeads.forEach( head=> createMyOwnElement('td', tr,"",user[head.name]) )
       const td = createMyOwnElement('td',tr)
       const delBtn = createMyOwnElement('button', td, "btn btn-danger mx-3", "delete")
+      delBtn.addEventListener('click', ()=> deleteUser(usersData, user.id))
       const editBtn = createMyOwnElement('button', td, "btn btn-warning mx-3", "Edit")
       const showBtn = createMyOwnElement('button', td, "btn btn-primary mx-3", "Show")
-      showBtn.addEventListener("click", (e)=> editUser(user.id))
     })    
     }
 }
-
-editUser = (id) =>{
-    alert(id)
+if(content) drawItems()
+deleteUser= (usersData, id, tr) =>{
+    newData = usersData.filter(u=> u.id != id)
+    setDataToStorage(newData)
+     drawItems()
 }
+
