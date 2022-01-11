eb4010f4ce3af3e4323ff07fe7a2a6ccb8e63fff
marwaMRadwan <marwaradwan872@hotmail.com>
2022-01-11 10:09:50 +0200

    s7

---
+++ session7/app.js
@@ -0,0 +1,22 @@
+//npm i --g nodemon
+const express= require("express")
+const path = require("path")
+const app = express()
+//__dirname , __filename => buildin variables
+//routes
+app.get("/", (req, res)=>{
+    res.send("hello from index page")
+})
+app.get("/hi", (req, res)=>{
+    res.send("hello")
+})
+app.get("/json", (req,res)=>{
+    let user = { name: "marwa"} 
+    res.send(user)
+})
+app.get("/test", (req,res)=>{
+    // res.sendFile("index.html")
+    let link = path.join(__dirname, "index.html")
+    res.sendFile(link)
+})
+app.listen(3000) //localhost:3000 3001
