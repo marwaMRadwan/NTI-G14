5965d61879e284b19aa84fb90873069d60b3c9fc
marwaMRadwan <marwaradwan872@hotmail.com>
2022-01-11 13:57:34 +0200

    s7

--- session7/expressApp/index.js
+++ session7/expressApp/index.js
@@ -27,8 +27,21 @@ app.get("/", (req,res)=>{
     // res.send(data)
     res.render('home', data)
 })
+//?x=5 =>query     /x =>param
+app.get("/images/:id", (req,res)=>{
+    // res.send(req.query) // req.params
+    let data = { pageTitle: "single image Page"}
+    let id = req.params.id 
+    let allData = JSON.parse(fs.readFileSync("data.json"))
+    data.img = allData.find(el=> el.id==id)
+    if(!data.img) res.redirect('/error')
+    res.render('single', data)
+})
 app.get("/about", (req,res)=>{
     let data = { pageTitle: "About Page"}
     res.render('about', data)
+})
+app.get("*", (req,res)=>{
+    res.render('err404', {pageTitle:"page not found"})
 })
 app.listen(3000, ()=>console.log("server on http://localhost:3000"))
