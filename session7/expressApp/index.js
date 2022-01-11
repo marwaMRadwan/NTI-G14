3b37352e57cbccde080b64c4e2918bd2c57a33cc
marwaMRadwan <marwaradwan872@hotmail.com>
2022-01-11 13:10:01 +0200

    s7

--- session7/expressApp/index.js
+++ session7/expressApp/index.js
@@ -2,6 +2,7 @@ const express = require("express")
 const hbs = require('hbs')
 const path = require("path")
 const app = express()
+const fs = require('fs')
 
 const publicDir = path.join(__dirname, "public")
 const viewsDir = path.join(__dirname, "frontEnd/views")
@@ -12,11 +13,22 @@ app.set('view engine', 'hbs')
 app.set('views', viewsDir)
 hbs.registerPartials(layOutsDir)
 
-app.get("/", (req, res)=>{
-    res.render('home', {name:"marwa"})
-})
-app.get("/about", (req, res)=>{
-    res.render('about')
-})
+// app.get("/", (req, res)=>{
+//     res.render('home', {name:"marwa"})
+// })
 
+// app.get("/about", (req, res)=>{
+//     res.render('about')
+// })
+
+app.get("/", (req,res)=>{
+    let data = { pageTitle: "Home Page"}
+    data.allData = JSON.parse(fs.readFileSync("data.json"))
+    // res.send(data)
+    res.render('home', data)
+})
+app.get("/about", (req,res)=>{
+    let data = { pageTitle: "About Page"}
+    res.render('about', data)
+})
 app.listen(3000, ()=>console.log("server on http://localhost:3000"))
