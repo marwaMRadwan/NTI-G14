c30e81be3237e7afa93e6862df6da00ff9e1adc3
marwaMRadwan <marwaradwan872@hotmail.com>
2022-01-12 11:08:29 +0200

    s8

--- session8/app/controller/app.controller.js
+++ session8/app/controller/app.controller.js
@@ -22,15 +22,25 @@ class User{
     static showAll = (req, res) => {
         res.render("all", {pageTitle: "All Users"})
     }
+    //get method
     static addUser = (req, res)=>{
-        // /add {} {name:1, age:2} [name, age]
        if(Object.keys(req.query).length != 0) {
             const data = readFromJSON()
-            data.push(req.query)
+            data.push(req.query) 
             writeDataToJSON(data)
             res.redirect("/")
         }
         res.render("add", {pageTitle:"Add New User"})
+    }
+    //post method
+    static addUserPost = (req, res)=>{
+        res.render("addPost", {pageTitle:"add new user(post)"})
+    }
+    static addUserLogic = (req,res)=>{
+        const data = readFromJSON()
+        data.push(req.body) 
+        writeDataToJSON(data)
+        res.redirect("/")
     }
     static editUser = (req,res)=>{
         res.render("edit", {pageTitle:"Edit User"})
--- session8/app/routes/app.routes.js
+++ session8/app/routes/app.routes.js
@@ -3,6 +3,10 @@ const User = require("../controller/app.controller")
 
 router.get("/", User.showAll)
 router.get("/add", User.addUser)
+
+router.get("/addPost", User.addUserPost)
+router.post("/addPost", User.addUserLogic)
+
 router.get("/edit/:id", User.editUser)
 router.get("/delete/:id", User.deleteUser)
 router.get("/single/:id", User.singleUser)
