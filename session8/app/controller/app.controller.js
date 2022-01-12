eb431fd2287d819bdf26108ed6817be7eda6ba46
marwaMRadwan <marwaradwan872@hotmail.com>
2022-01-12 10:45:45 +0200

    s8

--- session8/app/controller/app.controller.js
+++ session8/app/controller/app.controller.js
@@ -1,8 +1,35 @@
+const fs = require("fs")
+const readFromJSON = () =>{
+    let data
+    try{
+        data = JSON.parse(fs.readFileSync('./models/data.json'))
+        if(!Array.isArray(data)) throw new Error()
+    }
+    catch(e){
+        data = []
+    }
+    return data
+}
+const writeDataToJSON = (data) =>{
+    try{
+        fs.writeFileSync("./models/data.json", JSON.stringify(data))
+    }
+    catch(e){
+        console.log(chalk.red(e.message))
+    }
+}
 class User{
     static showAll = (req, res) => {
         res.render("all", {pageTitle: "All Users"})
     }
     static addUser = (req, res)=>{
+        // /add {} {name:1, age:2} [name, age]
+       if(Object.keys(req.query).length != 0) {
+            const data = readFromJSON()
+            data.push(req.query)
+            writeDataToJSON(data)
+            res.redirect("/")
+        }
         res.render("add", {pageTitle:"Add New User"})
     }
     static editUser = (req,res)=>{
