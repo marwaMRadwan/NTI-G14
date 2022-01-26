77855c2b676ba0a5adad932386c9f327778a0903
marwaMRadwan <marwaradwan872@hotmail.com>
2022-01-26 13:05:41 +0200

    s18

--- session18/src/app/pages/user/login/login.component.ts
+++ session18/src/app/pages/user/login/login.component.ts
@@ -1,31 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
-// import {environment} from 'src/environments/environment'
 import { AuthService } from 'src/app/providers/services/auth.service';
+import { Router } from "@angular/router"
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  // environment.flag = false   ==> invalid
-  // x= environment
-  user:any = {
-    email:"",
-    password:""
-  }
-  constructor(private _auth:AuthService) { 
+  user : any = { email:"", password:"" }
+  msg : string = ""
+  x : boolean = false
+  constructor(private _auth:AuthService, private router: Router) { 
     _auth.flag=false
   }
-
-  ngOnInit(): void {
-  }
-  handleLogin(data:NgForm){
+  ngOnInit(): void { }
+  onBlur() : void { this.x=true }
+  handleLogin(data:NgForm): void {
     if(data.valid){
-      console.log(this.user)
-      this._auth.login(this.user).subscribe(data=>console.log(data))
+      this._auth.login(this.user).subscribe(
+        (res) => { localStorage.setItem("proToken", res.data.token) } ,
+        (e)=>{ this.msg =  e.error.data },
+        ()=>{
+          this.msg=""
+          this.x=false
+          this._auth.flag=true
+          data.resetForm()
+          this.router.navigateByUrl('/profile')
+        }        
+      )
     }
   }
-  x=false
-  test(){ this.x=true }
 }
